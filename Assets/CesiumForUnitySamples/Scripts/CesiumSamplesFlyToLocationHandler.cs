8fe714ee990705dbfc5cbb1b217adb2bf7c71182
Janine Liu <janine.h.liu@gmail.com>
2023-04-19 15:37:31 -0400

    Refactor fly-to script, update tilesets and runtime settings for new API

--- Assets/CesiumForUnitySamples/Scripts/CesiumSamplesFlyToLocationHandler.cs
+++ Assets/CesiumForUnitySamples/Scripts/CesiumSamplesFlyToLocationHandler.cs
@@ -7,95 +7,96 @@ using Unity.Mathematics;
 using UnityEngine.InputSystem;
 #endif
 
-public class CesiumSamplesSubSceneManager : MonoBehaviour
+public class CesiumSamplesFlyToLocationHandler : MonoBehaviour
 {
-    [Tooltip("The CesiumSubScenes to fly between at runtime." +
-        "\n\n" +
-        "Note that this script only handles four CesiumSubScenes. " +
-        "These subscene locations can be flown to by pressing the " +
-        "1, 2, 3, 4 keys.")]
-    public List<CesiumSubScene> subScenes = new List<CesiumSubScene>();
+    [Tooltip("The CesiumFlyToController that will take flight at runtime.")]
+    public CesiumFlyToController flyToController;
+
+    [InspectorName("Locations (Longitude Latitude Height")]
+    [Tooltip("The locations in Longitude Latitude Height to fly between at runtime." +
+             "\n\n" +
+             "This script only handles four locations. These subscene locations can " +
+             "be flown to by pressing the 1, 2, 3, 4 keys.")]
+    public List<double3> locations = new List<double3>();
 
     [Tooltip("The desired yaw and pitch angles that the camera should have upon " +
-        "flying to the target CesiumSubScene." +
+        "flying to the target location." +
         "\n\n" +
         "The first element represents yaw, i.e. horizontal rotation or " +
         "rotation around the Y-axis.\n" +
         "The second element represents yaw, i.e. vertical rotation or " +
         "rotation around the Y-axis.\n" +
-        "If no value is provided for a sub-scene, Vector2.zero is used by default.")]
-    public List<Vector2> subSceneYawAndPitch = new List<Vector2>();
-
-    public CesiumFlyToController flyToController;
+        "If no value is provided for a location, Vector2.zero is used by default.")]
+    public List<Vector2> yawAndPitchAngles = new List<Vector2>();
 
     private void OnValidate()
     {
-        if(this.subScenes.Count > 4)
+        if (this.locations.Count > 4)
         {
-            this.subScenes.RemoveRange(4, this.subScenes.Count - 4);
+            this.locations.RemoveRange(4, this.locations.Count - 4);
         }
 
-        if (this.subSceneYawAndPitch.Count > this.subScenes.Count)
+        if (this.yawAndPitchAngles.Count > this.locations.Count)
         {
-            this.subSceneYawAndPitch.RemoveRange(
-                this.subScenes.Count - 1,
-                this.subSceneYawAndPitch.Count - this.subScenes.Count);
+            this.yawAndPitchAngles.RemoveRange(
+                this.locations.Count - 1,
+                this.yawAndPitchAngles.Count - this.locations.Count);
         }
     }
 
     void Update()
     {
-        if (this.subScenes.Count == 0)
+        if (this.locations.Count == 0)
         {
             return;
         }
 
         int? keyboardInput = GetKeyboardInput();
-        if(keyboardInput == null)
+        if (keyboardInput == null)
         {
             return;
         }
 
         int index = (int)keyboardInput - 1;
-        this.FlyToSubScene(index);
+        this.FlyToLocation(index);
     }
     static bool GetKey1Down()
     {
-        #if ENABLE_INPUT_SYSTEM
+#if ENABLE_INPUT_SYSTEM
         return Keyboard.current.digit1Key.isPressed || Keyboard.current.numpad1Key.isPressed;
-        #else
+#else
         return Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1);
-        #endif
+#endif
     }
 
     static bool GetKey2Down()
     {
-        #if ENABLE_INPUT_SYSTEM
+#if ENABLE_INPUT_SYSTEM
         return Keyboard.current.digit2Key.isPressed || Keyboard.current.numpad2Key.isPressed;
-        #else
+#else
         return Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2);
-        #endif
+#endif
     }
     static bool GetKey3Down()
     {
-        #if ENABLE_INPUT_SYSTEM
+#if ENABLE_INPUT_SYSTEM
         return Keyboard.current.digit3Key.isPressed || Keyboard.current.numpad3Key.isPressed;
-        #else
+#else
         return Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3);
-        #endif
+#endif
     }
     static bool GetKey4Down()
     {
-        #if ENABLE_INPUT_SYSTEM
+#if ENABLE_INPUT_SYSTEM
         return Keyboard.current.digit4Key.isPressed || Keyboard.current.numpad4Key.isPressed;
-        #else
+#else
         return Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4);
-        #endif
+#endif
     }
 
     static int? GetKeyboardInput()
     {
-        if(GetKey1Down())
+        if (GetKey1Down())
         {
             return 1;
         }
@@ -118,24 +119,20 @@ public class CesiumSamplesSubSceneManager : MonoBehaviour
         return null;
     }
 
-    void FlyToSubScene(int index)
+    void FlyToLocation(int index)
     {
-       CesiumSubScene subScene = this.subScenes[index];
-        double3 coordinatesECEF = new double3(
-            subScene.ecefX,
-            subScene.ecefY,
-            subScene.ecefZ);
+        double3 coordinatesLLH = this.locations[index];
 
         Vector2 yawAndPitch = Vector2.zero;
-        if(index < this.subSceneYawAndPitch.Count)
+        if (index < this.yawAndPitchAngles.Count)
         {
-            yawAndPitch = this.subSceneYawAndPitch[index];
+            yawAndPitch = this.yawAndPitchAngles[index];
         }
 
-        if(this.flyToController != null)
+        if (this.flyToController != null)
         {
-            this.flyToController.FlyToLocationEarthCenteredEarthFixed(
-                coordinatesECEF,
+            this.flyToController.FlyToLocationLongitudeLatitudeHeight(
+                coordinatesLLH,
                 yawAndPitch.x,
                 yawAndPitch.y,
                 true);
