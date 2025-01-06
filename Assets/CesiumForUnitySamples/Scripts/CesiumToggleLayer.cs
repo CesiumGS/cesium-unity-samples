de84bd72fe9a4e2a74556879403bf6c795108d5c
Ryan Veenstra <123468416+r-veenstra@users.noreply.github.com>
2025-01-06 14:18:48 +1000

    Add layer toggle

---
+++ Assets/CesiumForUnitySamples/Scripts/CesiumToggleLayer.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class CesiumToggleLayer : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    private Toggle toggle;
+
+    private void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+
+        if (toggle != null)
+        {
+            toggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+        else
+        {
+            Debug.LogError("CesiumToggleLayer requires a Toggle component on the same GameObject.");
+        }
+    }
+
+    private void Start()
+    {
+        if (target != null)
+        {
+            target.SetActive(toggle.isOn);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (toggle != null)
+        {
+            toggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+        }
+    }
+
+    private void OnToggleValueChanged(bool isOn)
+    {
+        if (target != null)
+        {
+            target.SetActive(isOn);
+        }
+    }
+}
